a11805692c2930c71952312d8eb9b2e07be1fae4
Aleksandr Demshin <demshin@gmail.com>
2022-10-24 17:26:20 +0500

    add dec2hex and bubble sort

--- src/lesson01.java
+++ src/lesson01.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class lesson01 {
 
     public static void main(String[] args) {
@@ -7,6 +9,10 @@ public class lesson01 {
         System.out.println(checkElementInArray(new int[]{1, 2, 3, 4, 5}, 3));
         System.out.println(checkElementInArray(new int[]{1, 2, 3, 4, 5}, 6));
         System.out.println(revertString("Hello world!"));
+        System.out.println(decToHex(10));
+        System.out.println(decToHex(15));
+        System.out.println(decToHex(2534255));
+        System.out.println(Arrays.toString(sortArray(new int[]{5, 2, 1, 4, -2000, 0, 433, -3})));
     }
 
     // get month name by number
@@ -36,5 +42,34 @@ public class lesson01 {
             result += str.charAt(i);
         }
         return result;
+    }
+
+    //decimal to hex
+    public static String decToHex(int dec) {
+        String hex = "";
+        while (dec > 0) {
+            int remainder = dec % 16;
+            if (remainder < 10) {
+                hex = remainder + hex;
+            } else {
+                hex = (char) (remainder + 55) + hex;
+            }
+            dec /= 16;
+        }
+        return hex;
+    }
+
+    //sort array by bubble method
+    public static int[] sortArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
     }
 }
